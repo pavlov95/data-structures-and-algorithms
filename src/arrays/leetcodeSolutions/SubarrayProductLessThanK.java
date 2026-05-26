872c7be56f638ce5fe8909d17a4b4375fffe05a5
pavlov95 <dim_pavlov@abv.bg>
2026-05-26 22:35:39 +0300

    add SubarrayProductLessThanK

---
+++ src/arrays/leetcodeSolutions/SubarrayProductLessThanK.java
@@ -0,0 +1,41 @@
+package arrays.leetcodeSolutions;
+
+//713. Subarray Product Less Than K
+
+//Given an array of integers nums and an integer k, return the number of contiguous subarrays where
+//the product of all the elements in the subarray is strictly less than k.
+
+
+public class SubarrayProductLessThanK {
+
+    //Time complexity: O(n)
+    //Space complexity: O(1)
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+
+        if(k<=1){
+            return 0;
+        }
+        int result = 0;
+
+        int product = 1;
+        //Left boundary of window
+        int left = 0;
+
+        //Expand the window using right pointer
+        for (int right = 0; right < nums.length; right++) {
+            product = product * nums[right];
+
+            //Shrink window from left to right
+            while (product >= k) {
+                product = product / nums[left];
+                left++;
+            }
+
+            //Every subarray ending at 'right' and starting from any index between
+            //left and right is valid.
+            result = result + right - left + 1;
+        }
+
+        return result;
+    }
+}
