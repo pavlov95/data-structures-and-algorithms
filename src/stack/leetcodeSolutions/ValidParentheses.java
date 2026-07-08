d709f751122f2fc4269b482083d817dbff33279c
pavlov95 <dim_pavlov@abv.bg>
2026-07-08 23:26:25 +0300

    add ValidParenthehsis solution

---
+++ src/stack/leetcodeSolutions/ValidParentheses.java
@@ -0,0 +1,37 @@
+package stack.leetcodeSolutions;
+
+import java.util.ArrayDeque;
+
+public class ValidParentheses {
+
+    public boolean isValid(String s) {
+        ArrayDeque<Character> stack = new ArrayDeque<>();
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+
+                char top = stack.pop();
+
+                if (ch == ')' && top != '(') {
+                    return false;
+                }
+
+                if (ch == '}' && top != '{') {
+                    return false;
+                }
+
+                if (ch == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
+
